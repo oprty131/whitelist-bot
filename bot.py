199086afcced798efbab3fd50032ce9b92c85bf6
oprty131 <slwmplylw@gmail.com>
2025-08-30 19:36:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -178,11 +178,17 @@ async def delta(interaction: discord.Interaction):
     except Exception as e:
         await interaction.followup.send(f"❌ Error: {e}")
 
+import discord
+from discord import app_commands
+import time
+import requests
+import asyncio
+
 cooldowns = {}
 
 @bot.tree.command(name="snipe", description="Stream Snipe Someone")
 @app_commands.describe(user_id="UserID", place_id="PlaceID")
-async def snipe(interaction: discord.Interaction, userid: int, placeid: int):
+async def snipe(interaction: discord.Interaction, user_id: int, place_id: int):
     user = interaction.user.id
     now = time.time()
 
@@ -205,18 +211,13 @@ async def snipe(interaction: discord.Interaction, userid: int, placeid: int):
         f"https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={user_id}&size=150x150&format=Png&isCircular=false"
     ).json()["data"][0]["imageUrl"]
 
-    place_data = requests.get(f"https://games.roblox.com/v1/games?universeIds={place_id}").json()
-    game_name_text = place_data.get("data", [{}])[0].get("name", "Unknown Game")
-    game_link = f"https://roblox.com/games/{place_id}"
-    game_name = f"[{game_name_text}]({game_link})"
-
     cursor = ""
     headers = {"User-Agent": "DiscordBot/1.0"}
     found_servers = []
 
     embed = discord.Embed(
         title=f"{user_id} | {username}",
-        description=f"Game: {game_name}\nPlace ID: {place_id}\nSearching servers...",
+        description=f"Place ID: {place_id}\nSearching servers...",
         color=discord.Color.green()
     )
     embed.set_thumbnail(url=target_thumb)
@@ -249,7 +250,7 @@ async def snipe(interaction: discord.Interaction, userid: int, placeid: int):
                     updated = True
 
         if updated:
-            desc = f"Game: {game_name}\nPlace ID: {place_id}\nFound in servers:\n"
+            desc = f"Place ID: {place_id}\nFound in servers:\n"
             for sid in found_servers:
                 desc += f"Join: [Click Here To Join](https://peeky.pythonanywhere.com/join?placeId={place_id}&gameInstanceId={sid})\n"
             embed.description = desc
@@ -261,7 +262,7 @@ async def snipe(interaction: discord.Interaction, userid: int, placeid: int):
         await asyncio.sleep(1.5)
 
     if not found_servers:
-        embed.description = f"Game: {game_name}\nPlace ID: {place_id}\n❌ Target not found in currently listed servers."
+        embed.description = f"Place ID: {place_id}\n❌ Target not found in currently listed servers."
         await msg.edit(embed=embed)
 
 @bot.tree.command(name="whitelist", description="Add a UserId to the whitelist")
